69c42880b7eb12c8f402aacdf41909ef69d8cea1
Anna Sonchak <kisukeurahara1328@gmail.com>
2023-04-01 23:28:48 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
